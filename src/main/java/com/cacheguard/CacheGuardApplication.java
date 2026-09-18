ddcbf9638188b5e23a96dc8f3618c6f92ce7a5ac
nilesh24rit <nilesh.rit.24@gmail.com>
2026-09-18 20:44:48 +0530

    Initialize Spring Boot project with base dependencies

---
+++ src/main/java/com/cacheguard/CacheGuardApplication.java
@@ -0,0 +1,12 @@
+package com.cacheguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheGuardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CacheGuardApplication.class, args);
+    }
+}
